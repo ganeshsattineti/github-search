06846246b1da88ce5a4ed652215eeda78d0c23ce
s ganesh <ganeshsattineti123@gmail.com>
2025-10-03 18:43:53 +0530

    git search code added

---
+++ index.js
@@ -0,0 +1,46 @@
+function getUsersData() {
+  var inputDomElement = document.querySelector("input");
+  var userName = inputDomElement.value;
+
+  // send get request to the github server
+  fetch(`https://api.github.com/users/${userName}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      var repos_url = data.repos_url;
+      var userImage = data.avatar_url;
+      var name = data.name;
+      var bio = data.bio;
+
+      document.querySelector("img").src = userImage;
+      document.querySelector("h3").innerText = name || `unknown Name`;
+      document.querySelector("p").innerText =
+        bio ||
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio exercitationem aut tenetur!";
+
+      fetch(repos_url)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          var reposdata = data.slice(0, 10);
+          var collection_lis = document.querySelectorAll("li");
+          collection_lis.forEach((li) => {
+            li.style.color = "red";
+            li.innerText = "no Repo's to display";
+          });
+
+          reposdata.forEach((repos, index) => {
+            collection_lis[index].innerText = repos.html_url;
+            collection_lis[index].style.color = "green";
+          });
+        });
+    })
+
+    .catch((error) => {
+      console.log("somenthing went wrong");
+    });
+}
